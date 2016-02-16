78f70e8cadcd2fa85e0291bf6ab379f3b85a8d99
Nick Pantic <nmpantic@cpp.edu>
2016-02-15 17:40:32 -0800

    first commit

---
+++ Conway.cpp
@@ -0,0 +1,9 @@
+#include "Conway.h"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+// add functions here
+
+// add main function here or in separate main.cpp
---
+++ Conway.h
@@ -0,0 +1,14 @@
+#ifndef CONWAY_H
+#define CONWAY_H
+
+#include <string>
+
+class Conway
+{
+private:
+    // add private members here
+public:
+    // add public menbers here
+};
+
+#endif
